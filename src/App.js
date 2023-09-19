7fb98a6530ea10d61a0b86dd16dfc7052cce0c7e
hi-rachel <rachel.uiux@gmail.com>
2023-09-19 22:15:02 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import ProductListing from "./components/ProductListing";
+import ProductDetail from "./components/ProductDetail";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing />} />
+          <Route path="/product/:productId" element={<ProductDetail />} />
+          <Route>404 Not Found!</Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
